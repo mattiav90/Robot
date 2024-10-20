2c552a764baabc868c2f4831993a402495b04e19
Mattia <mattia.vezzoli@yale.edu>
2024-10-20 09:58:48 -0400

    added teh start fresh

---
+++ set_up_fresh_image.sh
@@ -0,0 +1,12 @@
+# update
+sudo apt-get update
+
+# install opencv
+sudo apt-get install python3-opencv
+sudo apt-get remove python3-opencv
+
+# install code-oss (editor)
+sudo apt-get install curl
+curl -L https://github.com/toolboc/vscode/releases/download/1.32.3/code-oss_1.32.3-arm64.deb -o code-oss_1.32.3-arm64.deb
+sudo dpkg -i code-oss_1.32.3-arm64.deb
+

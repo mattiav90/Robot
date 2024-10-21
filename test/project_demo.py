007ff67b889bf6d512d02eb61e4caae12e00c19e
mattiav90 <mattia.vezzoli@yale.edu>
2024-10-20 20:47:40 -0400

    good progress

--- test/project_demo.py
+++ test/project_demo.py
@@ -34,7 +34,7 @@ cv2.moveWindow('cam2', 500, 500)
 
 cv2.createTrackbar('x1', 'cam', 110, 500, nothing)
 cv2.createTrackbar('x2', 'cam', 250, 500, nothing)
-cv2.createTrackbar('x3', 'cam', 150, 500, nothing)
+cv2.createTrackbar('x3', 'cam', 100, 500, nothing)
 cv2.createTrackbar('x4', 'cam', 255, 500, nothing)
 
 # Set a minimum area for contours to be considered "big"
@@ -52,7 +52,8 @@ min_area = 70  # You can adjust this value as needed
 #     plt.draw()  # Update the plot
 #     plt.pause(1)  # Pause to allow the plot to update
 
-kernel=np.ones((5,5),np.float32)/50
+kernel_soft=np.ones((5,5),np.float32)/20
+kernel_hard=np.ones((7,7),np.float32)/3
 
 
 while True:
@@ -69,11 +70,7 @@ while True:
     x2 = cv2.getTrackbarPos('x2', 'cam')
     x3 = cv2.getTrackbarPos('x3', 'cam')
     x4 = cv2.getTrackbarPos('x4', 'cam')
-    print(x1)
-    print(x2)
-    print(x3)
-    print(x4)
-
+    
 
     # Get dimensions
     height, width, _ = img.shape
@@ -88,27 +85,45 @@ while True:
     grey= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # blur a littlebit the image
-    kernel=np.ones((5,5),np.float32)/25
-    dst=cv2.filter2D(img,-1,kernel)
+    dst=cv2.filter2D(grey,-1,kernel_soft)
 
     # Step 1: Threshold the image
     _, thresh1 = cv2.threshold(dst, x3, x4, cv2.THRESH_BINARY)
 
-    # Ensure thresh1 is a single-channel 8-bit image
-    if len(thresh1.shape) != 2:
-        thresh1 = cv2.cvtColor(thresh1, cv2.COLOR_BGR2GRAY)
-
     # erode
-    thresh1= cv2.erode(thresh1,kernel,iterations = 1)
+    thresh1= cv2.erode(thresh1,kernel_hard,iterations = 1)
     # Step 3: Find contours
     contours, _ = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Step 4: Approximate the contours
     approx_contours = [cv2.approxPolyDP(c, 0.001 * cv2.arcLength(c, True), True) for c in contours]
 
+    x=0
     # Optionally, draw the approximated contours on the original image
     for c in approx_contours:
-        cv2.drawContours(img, [c], -1, (0, 0, 255), 2) 
+
+        x,y,w,h = cv2.boundingRect(c)
+        aspect_ratio = float(w)/h
+        area=cv2.contourArea(c)
+        if (aspect_ratio<0.5) and area>10000:
+            (x,y),(MA,ma),angle = cv2.fitEllipse(c)
+            ratio=ma/MA
+            # print("MA: ",MA," ma: ",ma," ratio: ",ratio)
+
+            if ratio>10:
+                cv2.drawContours(img, [c], -1, (0, 0, 255), 2) 
+                M = cv2.moments(c)
+
+                # Calculate the centroid coordinates
+                x = int(M['m10'] / M['m00'])
+                y = int(M['m01'] / M['m00'])
+                print("x: ",x)
+
+
+
+              
+
+
 
 
 

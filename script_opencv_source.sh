dba61a4c1541449cc96eab9dcd926d35c3ec553b
mattiav90 <mattia.vezzoli@yale.edu>
2024-10-23 21:22:39 -0400

    some more change

--- script_opencv_source.sh
+++ script_opencv_source.sh
@@ -2,7 +2,7 @@
 #
 
 version="4.5.0"
-folder="opencv"
+folder="opencv_install"
 
 cd ~
 
@@ -14,7 +14,7 @@ echo "** Install requirement"
 sudo apt-get update
 sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt-get install -y python2.7-dev python3.6-dev python-dev python-numpy python3-numpy
+sudo apt-get install -y python2.7-dev python3.8-dev python-dev python-numpy python3-numpy
 sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
 sudo apt-get install -y curl
@@ -33,7 +33,7 @@ cd opencv-${version}/
 echo "** Building..."
 mkdir release
 cd release/
-cmake -D WITH_CUDA=ON -D WITH_CUDNN=ON -D WITH_GDAL=ON -D WITH_PNG=ON -D WITH_JPEG=ON -D WITH_TIFF=ON -D CUDA_ARCH_BIN="5.3,6.2,7.2" -D CUDA_ARCH_PTX="" -D OPENCV_GENERATE_PKGCONFIG=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${version}/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+cmake -D WITH_CUDA=ON -D WITH_CUDNN=ON -D WITH_GDAL=ON -D WITH_GSTREAMER=ON -D WITH_V4L=ON -D WITH_GDAL=ON -D WITH_PNG=ON -D WITH_JPEG=ON -D WITH_TIFF=ON -D CUDA_ARCH_BIN="5.3,6.2,7.2" -D CUDA_ARCH_PTX="" -D OPENCV_GENERATE_PKGCONFIG=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-${version}/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make -j$(nproc)
 sudo make install
 

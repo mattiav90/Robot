df2d491b855ac628838bbd0d1ff362b6530441c2
mattiav90 <mattia.vezzoli@yale.edu>
2024-10-27 14:51:04 -0400

    some progress

--- set_up_fresh_image.sh
+++ set_up_fresh_image.sh
@@ -63,8 +63,18 @@ pip install FaBoPWM-PCA9685-Python/
 #then use the code in the file test_steer.py
 
 
-########## create a virtual environment  ##########
+########## configure th 40 pin of jetson nano  ##########
 
+$ sudo /opt/nvidia/jetson-io/jetson-io.py
+
+
+# Enable Pin 32 / PWM0
+busybox devmem 0x700031fc 32 0x45
+busybox devmem 0x6000d504 32 0x2
+
+# Enable Pin 33 / PWM2
+busybox devmem 0x70003248 32 0x46
+busybox devmem 0x6000d100 32 0x00
 
 
 

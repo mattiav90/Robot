9cf10530586ecda1f077a6a0fad2d83a96d6f008
mattiav90 <mattia.vezzoli@yale.edu>
2024-10-29 22:20:17 -0400

    cleaned up some stuff

---
+++ test/pwm_back_motors.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+import os
+# import keyboard  # Import the keyboard library
+
+# might be helpful
+# https://forums.developer.nvidia.com/t/nano-pwm0-cant-work/157462/11
+# https://forums.developer.nvidia.com/t/jetson-nano-pwm-not-working/154939/17
+
+
+# pin 33
+# sudo busybox devmem 0x70003248 32 0x46
+# sudo busybox devmem 0x6000d100 32 0x00
+# 
+# pin 32
+# sudo busybox devmem 0x700031fc 32 0x45
+# sudo busybox devmem 0x6000d504 32 0x2
+
+os.system("sudo busybox devmem 0x6000d504 32 0x2")
+os.system("sudo busybox devmem 0x700031fc 32 0x45")
+os.system("sudo busybox devmem 0x70003248 32 0x46")
+os.system("sudo busybox devmem 0x6000d100 32 0x00")
+
+
+#activate back motors
+
+# Setup
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(32, GPIO.OUT)
+
+# Start PWM
+my_pwm1 = GPIO.PWM(32, 100)  # 100 Hz
+my_pwm2 = GPIO.PWM(33, 100)  # 100 Hz
+duty_cycle = 50  # Initial duty cycle (10%)
+my_pwm1.start(duty_cycle)
+my_pwm2.start(duty_cycle)
+
+
+time.sleep(5)
+
+
+# Cleanup
+my_pwm1.stop()
+my_pwm2.stop()
+GPIO.cleanup()

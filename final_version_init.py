f8b62b59440de90b46e474388ae36ec5e4a8e63c
mattiav90 <mattia.vezzoli@yale.edu>
2025-01-28 21:16:19 -0500

    progress on the final script

--- final_version_init.py
+++ final_version_init.py
@@ -4,55 +4,93 @@ import matplotlib.pyplot as plt
 
 
 def plot_rgb(img,y_pos,id):
+
+    boola=True
     
     b,g,r = cv2.split(img)
     x_dim,y_dim,ch= img.shape
     scale = 2.7
     y_dim_plot = int(x_dim/scale)
     x_dim_plot = int(y_dim/scale)
+
+
+
     cv2.namedWindow(f"img {id}", cv2.WINDOW_NORMAL)
     cv2.resizeWindow(f"img {id}", x_dim_plot, y_dim_plot)
-    cv2.moveWindow(f"img {id}", x_dim_plot*0, y_pos)
     cv2.imshow(f"img {id}", img)
     # red img
     cv2.namedWindow(f"R {id}", cv2.WINDOW_NORMAL)
     cv2.resizeWindow(f"R {id}", x_dim_plot, y_dim_plot)
-    cv2.moveWindow(f"R {id}", x_dim_plot*1, y_pos)
     cv2.imshow(f"R {id}", r)
     # green img
     cv2.namedWindow(f"G {id}", cv2.WINDOW_NORMAL)
     cv2.resizeWindow(f"G {id}", x_dim_plot, y_dim_plot)
-    cv2.moveWindow(f"G {id}", x_dim_plot*2, y_pos)
     cv2.imshow(f"G {id}", g)
     # blu img
     cv2.namedWindow(f"B {id}", cv2.WINDOW_NORMAL)
     cv2.resizeWindow(f"B {id}", x_dim_plot, y_dim_plot)
-    cv2.moveWindow(f"B {id}", x_dim_plot*3, y_pos)
     cv2.imshow(f"B {id}", b)
 
+    if boola==True:
+        cv2.moveWindow(f"B {id}", x_dim_plot*3, y_pos)
+        cv2.moveWindow(f"G {id}", x_dim_plot*2, y_pos)
+        cv2.moveWindow(f"R {id}", x_dim_plot*1, y_pos)
+        cv2.moveWindow(f"img {id}", x_dim_plot*0, y_pos)
+    else:
+        boola=False
 
 
-def load_single_img(path):
-    """
-    Load a single image from the given path.
 
-    Args:
-        path (str): Path to the image.
+def plot_two2(imgG, imgB, roi_start, roi_height, contoursG, contoursB):
+    if imgG is None or imgB is None:
+        print("Error: One or both images are invalid.")
+        return
 
-    Returns:
-        img: Loaded image or None if the path doesn't exist or the image cannot be loaded.
-    """
-    if not os.path.exists(path):
-        print("The path:", path, "does not exist.")
-        return None
+    x_dim, y_dim = imgG.shape
+    scale = 2
+    x_dim_plot = int(x_dim / scale)
+    y_dim_plot = int(y_dim / scale)
+    print("x_dim_plot:", x_dim_plot, "y_dim_plot:", y_dim_plot)
 
-    img = cv2.imread(path)
-    if img is None:
-        print("Unable to load the image from:", path)
-    return img
+    # Convert grayscale images to BGR for visualization
+    imgG = cv2.cvtColor(imgG, cv2.COLOR_GRAY2BGR)
+    imgB = cv2.cvtColor(imgB, cv2.COLOR_GRAY2BGR)
+
+    # Translate contours by applying an offset to the y-coordinate
+    translated_contoursB = [contour + [0, roi_start] for contour in contoursB]
+    translated_contoursG = [contour + [0, roi_start] for contour in contoursG]
+
+    # Draw translated contours on the images
+    cv2.drawContours(imgB, translated_contoursB, -1, (255, 0, 0), 2)
+    cv2.drawContours(imgG, translated_contoursG, -1, (0, 255, 0), 2)
+
+    # Create named windows for resizing
+    cv2.namedWindow("imgB", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("imgB", y_dim_plot, x_dim_plot)
+    cv2.moveWindow("imgB",0,0)
+    cv2.imshow("imgB", imgB)
+
+    cv2.namedWindow("imgG", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("imgG", y_dim_plot, x_dim_plot)
+    cv2.moveWindow("imgG",int(y_dim_plot*1.15),0)
+    cv2.imshow("imgG", imgG)
 
 
-def load_img(path, display_time, calib_img):
+
+
+
+def merge_thresh_image(img,threshold,top,height,width):
+
+    # Create a copy of the original image to overlay the thresholded ROI
+    output_image = img.copy()   # Convert to BGR to display color
+    output_image[top:top+height, 0:width] = threshold  # Update the blue channel
+
+    return output_image
+
+
+
+
+def load_img(path, display_time):
     """
     Load and display images from the specified directory after subtracting a calibration image.
 
@@ -78,22 +116,91 @@ def load_img(path, display_time, calib_img):
 
         img = cv2.imread(img_full_path)
 
+
         if img is None:
             print(f"Unable to load image: {img_full_path}")
             continue
-
-        # Subtract the calibration image from the current image
-        sub = cv2.subtract(img, calib_img)
         
-        x_dim,y_dim,ch= sub.shape
+        height,width,ch= img.shape
+
+        # print("image shape: h: ",height," w: ",width)
+
+
+        # Calculate ROI dimensions
+        roi_width = int(width * 1.0)  # 100% of the image width
+        roi_height = int(height * 0.10)  # 15% of the image height
+
+        roi_start=int(height/100*70)
+
+        # Calculate top-left and bottom-right corners of the rectangle
+        top_left = (int((width - roi_width) // 2), roi_start)
+        # print(f"top_left: {top_left}")
+        bottom_right = (top_left[0] + roi_width, top_left[1] + roi_height)
+        # print(f"bottom_right: {bottom_right}")
+
+        # Draw the rectangle (blue color, thickness 2)
+        cv2.rectangle(img, top_left, bottom_right, (0, 0, 0), 1)
+
+        # split the image into BGR. 
+        imgB,imgG,imgR = cv2.split(img)
+
+        # define the rois
+        roiB = imgB[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+        roiG = imgG[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+
+
+
+        # blurra le roi dei canali
+        filter_size=25
+        roiB = cv2.GaussianBlur(roiB,(filter_size,filter_size),0)
+        roiG = cv2.GaussianBlur(roiG,(filter_size,filter_size),0)
+
+        # Calculate the average color in each channel (BGR format)
+        avgB = cv2.mean(roiB)[0]
+        avgG = cv2.mean(roiG)[0]
+        # avgR = cv2.mean(roiR)[0]
+        print(f"*avgB: {avgB} *avgG: {avgG} ")
+        # max min
+        (minValB, maxValB, _, _) = cv2.minMaxLoc(roiB)
+        (minValG, maxValG, _, _) = cv2.minMaxLoc(roiG)
+        # show this stuff
+        print(f"*maxValB: {maxValB} *maxValG: {maxValG} ")
+        print(f"*minValB: {minValB} *minValG: {minValG} ")
+        
+
+        
+
+
+        # threshold. define the thresholds considering the min max and avg. 
+
+        Blue_thresh  = max (100,int(avgB*1.25))
+        Green_thresh = max(20,int(avgG*3))
+
+        # probably there is no line in the image. do not detect a line
+        
+        if((maxValB-minValB)<50):
+            Blue_thresh=255
+        
+        if((maxValG-minValG)<20):
+            Green_thresh=255
+
+
+        # Apply Otsu's Thresholding for each channel
+        _, Bt = cv2.threshold(roiB, Blue_thresh, 255, cv2.THRESH_BINARY )
+        _, Gt = cv2.threshold(roiG, Green_thresh, 255, cv2.THRESH_BINARY )
+
+
+        # Detect contours for the blue threshold
+        contoursB, _ = cv2.findContours(Bt, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Detect contours for the green threshold
+        contoursG, _ = cv2.findContours(Gt, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
 
 
+        # plot the 2 images together. 
+        plot_two2(imgG,imgB,roi_start,roi_height,contoursG,contoursB)
 
-        # ******** plot ********
-        plot_rgb(sub,0,0)
-        plot_rgb(img,int(y_dim/3),1)
 
 
 
@@ -110,14 +217,7 @@ def load_img(path, display_time, calib_img):
 if __name__ == '__main__':
     # Path to the calibration image
     calib_path = "./imgs_saved/imgs1/calib.jpg"
-    calib = load_single_img(calib_path)
-    filter_size=11
-    blurred_calib = cv2.GaussianBlur(calib,(filter_size,filter_size),0)
 
-    if calib is not None:
-
-        # Path to the directory containing images
-        path = "./imgs_saved/imgs1/"
-        load_img(path, 0.1, calib)
-    else:
-        print("Calibration image could not be loaded.")
+    # Path to the directory containing images
+    path = "./imgs_saved/imgs1/"
+    load_img(path, 0.01)

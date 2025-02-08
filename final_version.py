20cb63fe366c8980e0bd763e6c2ecfc73e14bf83
mattiav90 <mattia.vezzoli@yale.edu>
2025-02-08 16:26:26 -0500

    good progress. just make sure they are always in the same order

--- final_version.py
+++ final_version.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 import os
-import pygame
 import Fabo_PCA9685
 import pkg_resources
 import smbus
+import argparse
 import cv2
 from collections import deque
 from builtins import open
@@ -96,7 +96,7 @@ def calculate_centroid(contours):
 
 def calculate_average_x(centroids):
     if len(centroids) == 0:
-        print("No centroids to calculate the average.")
+        # print("No centroids to calculate the average.")
         return None
     total_x = sum(cx for cx, cy in centroids)
     average_x = total_x / len(centroids)
@@ -165,169 +165,238 @@ def nothing(val):
     pass
 
 
-# define main plot window
-slider1_name = "sliderB"
-slider2_name = "sliderG"
-slider3_name = "sliderCR"
-slider4_name = "sliderSat"
+def check_if_exist(val):
+    out=0
+    if val:
+        out=val
+    else:
+        out=None
+    return out
 
-cv2.namedWindow("img", cv2.WINDOW_NORMAL)
-cv2.createTrackbar(slider1_name, "img", 0, 100, nothing)
-cv2.createTrackbar(slider2_name, "img", 0, 100, nothing)
-cv2.createTrackbar(slider3_name, "img", 0, 100, nothing)
-cv2.createTrackbar(slider4_name, "img", 0, 100, nothing)
-
-
-sliderB=0
-sliderG=0
-sliderCR=0
-sliderSat=0
-
-weigthed_center=0
-try:
-    while True:
-        
-        ret, img = cam.read()
-
-        # calculate roi
-        roi_width =  int(width*1.0)
-        roi_height = int(height*0.20)
-        roi_start=int(height/100*60)
-
-        # Calculate top-left and bottom-right corners of the rectangle
-        #  and draw the rectangle
-        top_left = (int((width - roi_width) // 2), roi_start)
-        bottom_right = (top_left[0] + roi_width, top_left[1] + roi_height)
-        cv2.rectangle(img, top_left, bottom_right, (0, 0, 0), 1)
-        
-        # estract RGB
-        imgB,imgG,imgR = cv2.split(img)
-
-        # Convert the image to HSV
-        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        img_sat = hsv_img[:, :, 1]
-
-        # Convert the image to YCrCb
-        ycrcb_img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
-        img_cr = ycrcb_img[:, :, 1]
-
-        # calculate the rois and apply them to the images
-        roiB    = imgB[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-        roiG    = imgG[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-        roiSat  = img_sat[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-        roiCR   = img_cr[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-        
-        #blur and extract avg, min, max
-        blur_filter = 25
-        avgB,minB,maxB       = img_roi(imgB,roiB,blur_filter)
-        avgG,minG,maxG       = img_roi(imgG,roiG,blur_filter)
-        avgSat,minSat,maxSat = img_roi(img_sat,roiSat,blur_filter)
-        avgCR,minCR,maxCR    = img_roi(img_cr,roiCR,blur_filter)
-        
-
-        # # threshold. define the thresholds considering the min max and avg. 
-        Blue_thresh  = int(avgB* (1+(1*sliderB/100)) )
-        Green_thresh = int(avgG* (1+(1*sliderG/100)) )
-        Sat_thresh   = int(avgG* (1+(1*sliderSat/100)) )
-        CR_thresh    = int(avgG* (1+(1*sliderCR/100)) )
-
-        # print("Blue_thresh: ",Blue_thresh)
-        # print("Green_thresh: ",Green_thresh)
-        # print("Sat_thresh: ",Sat_thresh)
-        # print("CR_thresh: ",CR_thresh)
-
-        # apply the threshold in each channel
-        _, Bt = cv2.threshold(roiB, Blue_thresh, 255, cv2.THRESH_BINARY )
-        _, Gt = cv2.threshold(roiG, Green_thresh, 255, cv2.THRESH_BINARY )
-
-        # detect the countours
-        contoursB, _ = cv2.findContours(Bt, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contoursG, _ = cv2.findContours(Gt, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        # Define a minimum and maximum area threshold
-        min_area = 200  # Adjust as needed
-        max_area = 30000  # Adjust as needed
-
-        # Filter contours by area for blue threshold with area printing
-        filtered_contoursB = filter_contour_area(contoursB,min_area,max_area)
-
-        # Filter contours by area for green threshold with area printing
-        filtered_contoursG = filter_contour_area(contoursG,min_area,max_area)
-
-        # trova i centroidi 
-        centroidsB = calculate_centroid(filtered_contoursB)
-        centroidsG = calculate_centroid(filtered_contoursG)
-
-        # average position of the centroids that I find. 
-        avgB_c=calculate_average_x(centroidsB)
-        avgG_c=calculate_average_x(centroidsG)
-
-        # modify the global centro variables if something has been detected
-        if avgB_c:
-            centroB = avgB_c
-        else:
-            centroB = None
-        
-        if avgG_c:
-            centroG = avgG_c
-        else:
-            centroG = None
-
-        if centroB and centroG:
-            weigthed_center = int( (centroG * 0.8 + centroB * 0.2)  )
-            # weigthed_center = int( (centroG * 0.8 + centroB * 0)  )
-        elif centroB:
-            weigthed_center = centroB
-        elif centroG:
-            weigthed_center = centroG
-        
-
-        cv2.circle(img, (weigthed_center, int(roi_start+roi_height/2)), 3, (0, 0, 255), 10)
-
-
-        if weigthed_center:
-            angle_avg=rolling_buffer(buffer, weigthed_center, ROLLING_BUFFER_SIZE)
-            servo_angle = int((angle_avg / width) * (SERVO_MAX - SERVO_MIN) + SERVO_MIN)
-            PCA9685.set_channel_value(channel, servo_angle)
-
-        
-
-        # define main plot
-        scale_img=1
-        cv2.resizeWindow("img", int(width/scale_img), int(height/scale_img))
-        cv2.moveWindow("img",0,0)
-        sliderB = cv2.getTrackbarPos(slider1_name,"img")
-        sliderG = cv2.getTrackbarPos(slider2_name,"img")
-        sliderCR = cv2.getTrackbarPos(slider3_name,"img")
-        sliderSat = cv2.getTrackbarPos(slider4_name,"img")
-   
-        
-
-        cv2.imshow("img",img)
-
-
-        # merge image with found contours
-        imgG = plot_nicely(imgG,roi_start,roi_height,filtered_contoursG,centroG,(0,255,0))
-        imgB = plot_nicely(imgB,roi_start,roi_height,filtered_contoursB,centroB,(255,0,0))
-
-        
-        # plot image with detected contours
-        single_plot(imgB,"imgB",2.5,[1000,0])
-        single_plot(imgG,"imgG",2.5,[1000,400])
+def print4(val1,val2,val3,val4):
+    print(f"val1: {val1} val2: {val2} val3: {val3} val4: {val4}")
+    return 0
 
 
 
-        if cv2.waitKey(1) == ord('q'):
-            break
+def go(sim):
+
+    # define main plot window and trackbars in it.
+    cv2.namedWindow("img", cv2.WINDOW_NORMAL)
+    cv2.moveWindow("img",0,0)
+    slider1_name = "sliderB"
+    slider2_name = "sliderG"
+    slider3_name = "sliderCR"
+    slider4_name = "sliderSat"
+    cv2.createTrackbar(slider1_name, "img", 0, 100, nothing)
+    cv2.createTrackbar(slider2_name, "img", 0, 100, nothing)
+    cv2.createTrackbar(slider3_name, "img", 0, 100, nothing)
+    cv2.createTrackbar(slider4_name, "img", 0, 100, nothing)
+    sliderB=0
+    sliderG=0
+    sliderCR=0
+    sliderSat=0
+
+    print("sim: ",sim)
+
+
+    path = "imgs_saved/imgs1/"
+    images = sorted(os.listdir(path))
+
+
+    weigthed_center=0
+    i=0
+    try:
+        while True:
+
+            # if I want to run a simulation
+            if args.sim:
+                i+=1
+                if i==len(images):
+                    i=1
+                img_name=f"{i+1}.jpg"
+                full_path = os.path.join(path,img_name)
+                img=cv2.imread(full_path)
+            
+            # if it is using the cam
+            else:
+                ret, img = cam.read()
+
+            # calculate roi
+            roi_width =  int(width*1.0)
+            roi_height = int(height*0.20)
+            roi_start=int(height/100*60)
+
+            # Calculate top-left and bottom-right corners of the rectangle
+            #  and draw the rectangle
+            top_left = (int((width - roi_width) // 2), roi_start)
+            bottom_right = (top_left[0] + roi_width, top_left[1] + roi_height)
+            cv2.rectangle(img, top_left, bottom_right, (0, 0, 0), 1)
+            
+            # estract RGB
+            imgB,imgG,imgR = cv2.split(img)
+
+            # Convert the image to HSV
+            hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            imgSat = hsv_img[:, :, 1]
+            imgSat = 255-imgSat
+
+            # Convert the image to YCrCb
+            ycrcb_img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+            imgCR = ycrcb_img[:, :, 1]
+            imgCR = 255 - imgCR
+
+            # calculate the rois and apply them to the images
+            roiB    = imgB[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+            roiG    = imgG[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+            roiSat  = imgSat[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+            roiCR   = imgCR[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+            
+            #blur and extract avg, min, max
+            blur_filter = 25
+            avgB,minB,maxB       = img_roi(imgB,roiB,blur_filter)
+            avgG,minG,maxG       = img_roi(imgG,roiG,blur_filter)
+            avgSat,minSat,maxSat = img_roi(imgSat,roiSat,blur_filter)
+            avgCR,minCR,maxCR    = img_roi(imgCR,roiCR,blur_filter)
 
 
 
-# closing up everything
-finally:
-    print("Cleaning up resources...")
-    my_pwm1.stop()
-    my_pwm2.stop()
-    GPIO.cleanup()
-    cam.release()
-    cv2.destroyAllWindows()
-    pygame.quit()
+            # print4(avgB,avgG,avgSat,avgCR)
+            
+
+            # # threshold. define the thresholds considering the min max and avg. 
+            Blue_thresh  = int(avgB*   (1+(1*sliderB/100)) )
+            Green_thresh = int(avgG*   (1+(1*sliderG/100)) )
+            Sat_thresh   = int(avgSat* (1+(1*sliderSat/100)) )
+            CR_thresh    = int(avgCR*  (1+(1*sliderCR/100)) )
+
+            # print4(Blue_thresh,Green_thresh,Sat_thresh,CR_thresh)
+
+
+            # apply the threshold in each channel
+            _, Bt   = cv2.threshold(roiB,   Blue_thresh, 255, cv2.THRESH_BINARY )
+            _, Gt   = cv2.threshold(roiG,   Green_thresh, 255, cv2.THRESH_BINARY )
+            _, Satt = cv2.threshold(roiSat, Sat_thresh, 255, cv2.THRESH_BINARY )
+            _, CRt  = cv2.threshold(roiCR,  CR_thresh, 255, cv2.THRESH_BINARY )
+
+            # detect the countours
+            contoursB, _   = cv2.findContours(Bt, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contoursG, _   = cv2.findContours(Gt, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contoursSat, _ = cv2.findContours(Satt, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contoursCR, _  = cv2.findContours(CRt, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            # Define a minimum and maximum area threshold
+            min_area = 200  # Adjust as needed
+            max_area = 30000  # Adjust as needed
+
+            # Filter contours by area for blue threshold with area printing
+            filtered_contoursB   = filter_contour_area(contoursB,min_area,max_area)
+            filtered_contoursG   = filter_contour_area(contoursG,min_area,max_area)
+            filtered_contoursSat = filter_contour_area(contoursSat,min_area,max_area)
+            filtered_contoursCR  = filter_contour_area(contoursCR,min_area,max_area)
+
+
+
+            # trova i centroidi 
+            centroidsB   = calculate_centroid(filtered_contoursB)
+            centroidsG   = calculate_centroid(filtered_contoursG)
+            centroidsSat = calculate_centroid(filtered_contoursSat)
+            centroidsCR  = calculate_centroid(filtered_contoursCR)
+
+            # print4(centroidsB,centroidsG,centroidsSat,centroidsCR)
+
+            # average position of the centroids that I find. 
+            avgB_c=calculate_average_x(centroidsB)
+            avgG_c=calculate_average_x(centroidsG)
+            avgSat_c=calculate_average_x(centroidsSat)
+            avgCR_c=calculate_average_x(centroidsCR)
+
+            # print4(avgB_c,avgG_c,avgSat_c,avgCR_c)
+
+            # modify the global centro variables if something has been detected
+            centroB = check_if_exist(avgB_c)
+            centroG = check_if_exist(avgG_c)
+            centroSat = check_if_exist(avgSat_c)
+            centroCR = check_if_exist(avgCR_c)
+
+            # print("centroB: ",centroB," centroG: ",centroG," centroSat: ",centroSat," centroCR: ",centroCR )
+            
+
+            if centroB and centroG:
+                weigthed_center = int( (centroG * 0.8 + centroB * 0.2)  )
+                # weigthed_center = int( (centroG * 0.8 + centroB * 0)  )
+            elif centroB:
+                weigthed_center = centroB
+            elif centroG:
+                weigthed_center = centroG
+            
+
+            cv2.circle(img, (weigthed_center, int(roi_start+roi_height/2)), 3, (0, 0, 255), 10)
+
+
+            if weigthed_center:
+                angle_avg=rolling_buffer(buffer, weigthed_center, ROLLING_BUFFER_SIZE)
+                servo_angle = int((angle_avg / width) * (SERVO_MAX - SERVO_MIN) + SERVO_MIN)
+                PCA9685.set_channel_value(channel, servo_angle)
+
+            
+
+            # define main plot
+            scale_img=1
+            cv2.resizeWindow("img", int(width/scale_img), int(height/scale_img))
+            sliderB = cv2.getTrackbarPos(slider1_name,"img")
+            sliderG = cv2.getTrackbarPos(slider2_name,"img")
+            sliderCR = cv2.getTrackbarPos(slider3_name,"img")
+            sliderSat = cv2.getTrackbarPos(slider4_name,"img")
+
+            
+
+            # cv2.imshow("img",img)
+
+
+            # merge image with found contours
+            imgG   = plot_nicely(imgG,roi_start,roi_height,filtered_contoursG,centroG,(0,255,0))
+            imgB   = plot_nicely(imgB,roi_start,roi_height,filtered_contoursB,centroB,(255,0,0))
+            imgSat = plot_nicely(imgSat,roi_start,roi_height,filtered_contoursSat,centroB,(0,0,255))
+            imgCR  = plot_nicely(imgCR,roi_start,roi_height,filtered_contoursCR,centroB,(0,0,0))
+
+            scale_subplot=2.8
+            offset=int(width/scale_subplot)
+            # plot image with detected contours
+            single_plot(imgB,"imgB",scale_subplot     ,[offset*0,0] )
+            single_plot(imgG,"imgG",scale_subplot     ,[offset*1,0] )
+            single_plot(imgSat,"imgSat",scale_subplot ,[offset*2,0] )
+            single_plot(imgCR,"imgCR",scale_subplot   ,[offset*3,0] )
+
+
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+
+
+    # closing up everything
+    finally:
+        print("Cleaning up resources...")
+        my_pwm1.stop()
+        my_pwm2.stop()
+        GPIO.cleanup()
+        cam.release()
+        cv2.destroyAllWindows()
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simulation=<bool>")
+    parser.add_argument("--sim", type=bool, help="Enable simulation mode.",default=False)
+    args = parser.parse_args()
+
+    if args.sim:
+        print("Simulation run")
+    else:
+        print("Real run")
+
+    # launch the main function
+    go(args.sim)
+
